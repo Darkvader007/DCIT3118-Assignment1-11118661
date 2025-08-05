2e83194d42048f97199d37b9ce1d1919a07f5bc1
Gabe <gkaddo>
2025-08-05 13:38:32 +0000

    Added a Grade class

---
+++ ConsoleApp1/Grade.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   
+        public class Grade
+        {
+
+            public int grade;
+
+
+            public void Checkgrade()
+            {
+                Console.WriteLine("Enter Your Grade (from 0 - 100):");
+                int grade = Convert.ToInt32(Console.ReadLine());
+
+                if (grade < 0 || grade > 100)
+                {
+                    Console.WriteLine("Invalid Grade");
+                }
+                else if (grade >= 90)
+                {
+                    Console.WriteLine(" Grade A");
+                }
+                else if (grade > 79 && grade < 90)
+                {
+                    Console.WriteLine(" Grade B");
+                }
+                else if (grade > 69 && grade < 80)
+                {
+                    Console.WriteLine(" Grade C");
+                }
+                else if (grade > 59 && grade < 70)
+                {
+                    Console.WriteLine("Grade D");
+                }
+                else
+                {
+                    Console.WriteLine(" Grade F");
+                }
+
+            }
+        }
+
+    
+}
